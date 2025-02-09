51a5301e48838a91b440b5a0f1d127f8f6d50050
CloodDev <cloodowy@gmail.com>
2025-02-09 15:05:52 +0100

    Better mod detection and better ui

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,71 +1,121 @@
-import type {AppInitConfig} from './AppInitConfig.js';
-import {createModuleRunner} from './ModuleRunner.js';
-import {disallowMultipleAppInstance} from './modules/SingleInstanceApp.js';
-import {createWindowManagerModule} from './modules/WindowManager.js';
-import {terminateAppOnLastWindowClose} from './modules/ApplicationTerminatorOnLastWindowClose.js';
-import {hardwareAccelerationMode} from './modules/HardwareAccelerationModule.js';
-import {autoUpdater} from './modules/AutoUpdater.js';
-import {allowInternalOrigins} from './modules/BlockNotAllowdOrigins.js';
-import {allowExternalUrls} from './modules/ExternalUrls.js';
-import { dialog } from 'electron';
-import * as fs from 'node:fs';
+import type { AppInitConfig } from "./AppInitConfig.js";
+import { createModuleRunner } from "./ModuleRunner.js";
+import { disallowMultipleAppInstance } from "./modules/SingleInstanceApp.js";
+import { createWindowManagerModule } from "./modules/WindowManager.js";
+import { terminateAppOnLastWindowClose } from "./modules/ApplicationTerminatorOnLastWindowClose.js";
+import { hardwareAccelerationMode } from "./modules/HardwareAccelerationModule.js";
+import { autoUpdater } from "./modules/AutoUpdater.js";
+import { allowInternalOrigins } from "./modules/BlockNotAllowdOrigins.js";
+import { allowExternalUrls } from "./modules/ExternalUrls.js";
+import { dialog } from "electron";
+import * as fs from "node:fs";
+import { shell } from "electron";
 export async function initApp(initConfig: AppInitConfig) {
   const moduleRunner = createModuleRunner()
-    .init(createWindowManagerModule({initConfig, openDevTools: import.meta.env.DEV}))
+    .init(
+      createWindowManagerModule({
+        initConfig,
+        openDevTools: import.meta.env.DEV,
+      })
+    )
     .init(disallowMultipleAppInstance())
     .init(terminateAppOnLastWindowClose())
-    .init(hardwareAccelerationMode({enable: false}))
+    .init(hardwareAccelerationMode({ enable: false }))
     .init(autoUpdater())
 
     // Install DevTools extension if needed
     // .init(chromeDevToolsExtension({extension: 'VUEJS3_DEVTOOLS'}))
 
-    .init(allowInternalOrigins(
-      new Set(initConfig.renderer instanceof URL ? [initConfig.renderer.origin] : []),
-    ))
-    .init(allowExternalUrls(
-      new Set(
-        initConfig.renderer instanceof URL
-          ? [
-            'https://vite.dev',
-            'https://developer.mozilla.org',
-            'https://solidjs.com',
-            'https://qwik.dev',
-            'https://lit.dev',
-            'https://react.dev',
-            'https://preactjs.com',
-            'https://www.typescriptlang.org',
-            'https://vuejs.org',
-          ]
-          : [],
-      )),
+    .init(
+      allowInternalOrigins(
+        new Set(
+          initConfig.renderer instanceof URL ? [initConfig.renderer.origin] : []
+        )
+      )
+    )
+    .init(
+      allowExternalUrls(
+        new Set(
+          initConfig.renderer instanceof URL
+            ? [
+                "https://vite.dev",
+                "https://developer.mozilla.org",
+                "https://solidjs.com",
+                "https://qwik.dev",
+                "https://lit.dev",
+                "https://react.dev",
+                "https://preactjs.com",
+                "https://www.typescriptlang.org",
+                "https://vuejs.org",
+              ]
+            : []
+        )
+      )
     );
   await moduleRunner;
 }
 
-
-import { ipcMain } from 'electron';
-ipcMain.handle('select-directory', async (event, operation) => {
-  const properties: Array<'openDirectory' | 'createDirectory'> = operation === 'export' ? ['openDirectory', 'createDirectory'] : ['openDirectory'];
+import { ipcMain } from "electron";
+ipcMain.handle("select-directory", async (event, operation) => {
+  const properties: Array<"openDirectory" | "createDirectory"> =
+    operation === "export"
+      ? ["openDirectory", "createDirectory"]
+      : ["openDirectory"];
   const result = await dialog.showOpenDialog({
-      properties: properties
+    properties: properties,
   });
   if (result.canceled) {
-      return null;
+    return null;
   } else {
-      return result.filePaths[0];
+    return result.filePaths[0];
   }
 });
 
-
 ipcMain.handle("list-mods", async (event, operation) => {
-  console.log(operation);
+  if (operation === 'C:/'){
+    return [];
+  }
+  try {
   const mods = fs.readdirSync(operation + "/mods");
-  return mods;
+  mods.forEach((mod, index) => {
+    if (mods[index].includes("saves")) {
+      mods.splice(index, 1);
+    }
+    if (mods[index].includes("logs")) {
+      mods.splice(index, 1);
+    }
+    if (mods[index].includes("downloads")) {
+      mods.splice(index, 1);
+    }
+    if (mods[index].includes("base")) {
+      mods.splice(index, 1);
+    }
+  });
+  return mods;} catch (err) {
+    return [];
+  }
+});
+
+ipcMain.handle("get-mod-data", async (event, operation) => {
+  try {
+    let modText = fs.readFileSync(operation + "/mod.txt", "utf8");
+    let mod: { name: string; version: string; author: string } = JSON.parse(modText);
+    let mdata = {
+      name: mod.name,
+      version: mod.version,
+      author: mod.author,
+    }
+    return mdata;
+  } catch (err) {
+    return "No mod.txt found";
+  }
+});
+
+ipcMain.handle("open-mod-folder", async (event, operation) => {
+  shell.openPath(operation);
 });
 
 ipcMain.handle("load-options", async (event, operation) => {
-  console.log(operation);
   const mods = fs.readdirSync(operation);
   return mods;
 });
--- packages/renderer/src/App.jsx
+++ packages/renderer/src/App.jsx
@@ -4,52 +4,98 @@ import './App.css'
 function App() { //
   const [path, setPath] = useState('');
   const [modList, setModList] = useState([]);
+  const [selected, setSelected] = useState(-1);
+  const [selectedModData, setSelectedModData] = useState([]);
+  useEffect(() => {
+    if (path !== 'C:\\mods') {
+      ListMods();
+    }
+  }, [path])
 
   useEffect(() => {
-    if (path === '') {
-      LoadSettings()
+    if (selected !== -1) {
+      window.electron.ipcRenderer.invoke('get-mod-data', path + "/mods/" + modList[selected]).then((data) => {
+        setSelectedModData(data);
+      });
     }
-  }, [])
-
-  async function LoadSettings() {
-    const settings = await window.electron.ipcRenderer.invoke('load-settings', "./settings.json");
-    console.log(settings);
-    await setPath(settings.path);
-    ListMods();
-  }
+  }, [selected])
 
   async function SelectDir() {
     const exportPath = await window.electron.ipcRenderer.invoke('select-directory', 'export');
     setPath(exportPath);
     ListMods();
+    return exportPath;
+  }
+
+  function GetAuthor() {
+    if (selectedModData.author) {
+      return selectedModData.author;
+    }
+    else {
+      return 'Unknown';
+    }
+  }
+  function GetVersion() {
+    if (selectedModData.version) {
+      return selectedModData.version;
+    }
+    else {
+      return 'Incorrectly Formatted or Beardlib Mod';
+    }
+  }
+  function GetName() {
+    if (selectedModData.name) {
+      return selectedModData.name;
+    }
+    else {
+      return modList[selected];
+    }
   }
   async function ListMods() {
     setModList(await window.electron.ipcRenderer.invoke('list-mods', path));
   }
-  return (
-    <>
-      <div className="modList">
-        {modList.map((modName, modID) => (
-          <div className="mod" key={modID}>
-            <div className="modName">{modName}</div>
+  async function OpenFolder() {
+    await window.electron.ipcRenderer.invoke('open-mod-folder', path + "/mods/" + modList[selected]);
+  }
+  return (<div className='root'>
+    <div className="pathSelector">
+      <input
+        type="text"
+        id="pathInput"
+        placeholder="Payday 2 path..."
+        readOnly
+        onClick={() => SelectDir()}
+        value={path}
+      />
+    </div>
+    <div className='content'>
+      {modList.length > 0 ? (
+        <>
+          <div className="modList">
+            {modList.map((modName, modID) => (
+              <div className={`mod ${modID === selected ? 'selected' : ''}`} onClick={() => setSelected(modID)} key={modID}>
+                <div className="modName">{modName}</div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="rightList">
-        <div className="pathSelector">
-          <label htmlFor="pathInput">Select Path:</label>
-          <input
-            type="text"
-            id="pathInput"
-            placeholder="Enter path..."
-            readOnly
-            onClick={() => SelectDir()}
-            value={path}
-          />
-          <button onClick={() => ListMods()}></button>
-        </div>
-      </div>
-    </>
+          <div className="rightList">
+            <div className="modInfo">
+              {selected !== -1 && (
+                <div>
+                  <h2>{GetName()}</h2>
+                  <p>Author: {GetAuthor()}</p>
+                  <p>Version: {GetVersion()}</p>
+                  <button onClick={() => OpenFolder()}>Open Mod Folder</button>
+                </div>
+              )}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>No mods found</div>
+      )}
+    </div>
+  </div>
   )
 }
 
